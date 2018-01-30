e4d09f5165d063ad518c3fea509ea27adc335f3b
Russ <russ@marks-imac.lan>
2018-01-29 20:32:09 -0500

    Minor tweaks to macbuild.sh

--- macbuild.sh
+++ macbuild.sh
@@ -2,8 +2,9 @@ cp macpackage.json package.json
 npm install
 rm -rf out/make/*
 electron-forge --verbose make
-unzip out/make/TRIGGERcmdAgent*.zip -d ./out/make
+unzip out/make/TRIGGERcmdAgent*.zip -d ./out/make -q
 rm -rf out/make/TRIGGERcmdAgent.app/Contents/Resources/app/node_modules/fsevents/build/Release/.deps/private/var/folders/x_/0d9jrssn7s5glyjmpkhtszpm0000gp/T/electron-packager/darwin-x64/TRIGGERcmdAgent-darwin-x64/Electron.app
+echo Signing TRIGGERcmdAgent.app
 electron-osx-sign ./out/make/TRIGGERcmdAgent.app
 codesign -dv out/make/TRIGGERcmdAgent.app
 cd out/make
