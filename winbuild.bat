d7e4d03c109e446221f3405ecd11349a0721c033
Russell VanderMey <russell@vandermey.us>
2018-02-17 12:40:37 -0500

    Added winbuild.bat to copy winpackage.json to package.json

---
+++ winbuild.bat
@@ -0,0 +1,11 @@
+sc stop triggercmdagent.exe
+sc delete triggercmdagent.exe
+timeout /t 5
+rd /s /q C:\triggercmdDev\github\TRIGGERcmd-Agent\src\daemon
+copy /y c:\triggercmdDev\bkup\passthrough.js C:\triggercmdDev\github\TRIGGERcmd-Agent\node_modules\electron-compilers\lib
+cd C:\triggercmdDev\github\TRIGGERcmd-Agent
+copy winpackage.json package.json
+del src\*.cfg
+del src\*.tkn
+del src\*.log
+electron-forge make
