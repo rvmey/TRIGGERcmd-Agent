d51c560a1b944d61fd297d3f8ca2928208d58589
VanderMey Consulting <vanmeyconsulting@gmail.com>
2018-08-25 13:00:49 -0400

    Added the ability to use the Edit menu items - particularly to paste in the token

--- src/menu.js
+++ src/menu.js
@@ -40,13 +40,13 @@ module.exports = () => {
         {
         label: "Edit",
         submenu: [
-            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
-            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:", role: 'undo' },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:", role: 'redo' },
             { type: "separator" },
-            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
-            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:", role: 'cut' },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:", role: 'copy' },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:", role: 'paste' },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:", role: 'selectall' }
         ]
         }
     ];
