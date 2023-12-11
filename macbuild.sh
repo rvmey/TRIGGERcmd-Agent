ee39fdc54d8db71ac381060c498d2b3fdeacf044
vel1kan <TriggerCMD@proton.me>
2023-12-11 18:09:32 -0500

    Modified macbuild related scripts

--- macbuild.sh
+++ macbuild.sh
@@ -1,13 +1,5 @@
 cp macpackage.json package.json
+apk add git dpkg fakeroot rpm
 npm install
 rm -rf out/make/*
-# npm install -g electron-forge
-electron-forge --verbose make
-unzip -q out/make/TRIGGERcmdAgent*.zip -d ./out/make
-rm -rf out/make/TRIGGERcmdAgent.app/Contents/Resources/app/node_modules/fsevents/build/Release/.deps/private/var/folders/x_/0d9jrssn7s5glyjmpkhtszpm0000gp/T/electron-packager/darwin-x64/TRIGGERcmdAgent-darwin-x64/Electron.app
-echo Signing TRIGGERcmdAgent.app
-electron-osx-sign ./out/make/TRIGGERcmdAgent.app
-codesign -dv out/make/TRIGGERcmdAgent.app
-cd out/make
-create-dmg TRIGGERcmdAgent.app
-cd ../..
+npm run make
