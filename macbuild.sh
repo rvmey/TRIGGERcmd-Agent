ff106b03cea811291d30860f7b62ae139cbd9ee6
vel1kan <TriggerCMD@proton.me>
2023-12-11 19:55:17 -0500

    Updated macbuild to accept zip files

--- macbuild.sh
+++ macbuild.sh
@@ -2,7 +2,7 @@ cp macpackage.json package.json
 npm install
 rm -rf out/make/*
 npm run make
-unzip -q out/make/TRIGGERcmdAgent*zip -d ./out/make
+unzip -q out/make/zip/darwin/x64/*.zip -d ./out/make
 echo Signing TRIGGERcmdAgent.app
 electron-osx-sign ./out/make/TRIGGERcmdAgent.app
 codesign -dv out/make/TRIGGERcmdAgent.app
