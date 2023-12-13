c99d52bebfe93f7fc2d77ccbd04f8501402a19aa
Russell VanderMey <russell@vandermey.us>
2023-12-12 22:00:29 -0500

    Add osxNotarize

--- forge.config.js
+++ forge.config.js
@@ -18,7 +18,13 @@ module.exports = {
       "CompanyName": "TRIGGERcmd",
       "OriginalFilename": "TRIGGERcmdAgent.exe"
     },
-    osxSign: {}
+    osxSign: {},
+    osxNotarize: {
+      tool: 'notarytool',
+      appleId: process.env.APPLE_ID,
+      appleIdPassword: process.env.APPLE_PASSWORD,
+      teamId: process.env.APPLE_TEAM_ID
+    }
   },
   rebuildConfig: {},
   makers: [
