a43428a16f9124ad40fbb6d0fb4f655b36e98087
ve1ikan <143457873+ve1ikan@users.noreply.github.com>
2024-12-26 10:10:14 -0500

    Windows build improvement

---
+++ winsign.bat
@@ -0,0 +1,9 @@
+cd out\make\squirrel.windows\x64
+for %%f in (*.exe) do (
+    ren "%%f" "TRIGGERcmdAgentSetup.exe"
+)
+
+@REM "C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe" sign /sha1 b0708fb9253fc08989517657a6c6dbb53eb43be0 /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "VanderMey Consulting LLC" "C:\triggercmdDev\github\TRIGGERcmd-Agent\out\make\squirrel.windows\x64\TRIGGERcmdAgent-1.0.%1 Setup.exe"
+"D:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x64\signtool.exe" sign /sha1 b0708fb9253fc08989517657a6c6dbb53eb43be0 /tr http://timestamp.sectigo.com /td sha256 /fd sha256 /n "VanderMey Consulting LLC" ".\TRIGGERcmdAgentSetup.exe"
+
+start .
