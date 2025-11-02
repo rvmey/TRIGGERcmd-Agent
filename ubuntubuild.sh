21008615865560580613db65af19f417bb45c08c
Russell VanderMey <russell@vandermey.us>
2025-11-02 09:11:14 -0500

    try this

--- ubuntubuild.sh
+++ ubuntubuild.sh
@@ -6,6 +6,11 @@ apt install jq fakeroot dpkg rpm git -y
 # Linux deb and rpm
 cp ubuntupackage.json package.json
 npm i -g @electron-forge/cli
+npm i -g @electron-forge/maker-squirrel
+npm i -g @electron-forge/maker-deb
+npm i -g @electron-forge/maker-rpm
+npm i -g @electron-forge/maker-zip
+npm i -g @electron-forge/maker-dmg
 npm install
 rm -rf out/make/*
 npm run make
