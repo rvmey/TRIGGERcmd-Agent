6eee22e17b39a231cefa473403779095a3531069
Russell VanderMey <russell@vandermey.us>
2025-11-02 09:31:27 -0500

    Try this2

--- ubuntubuild.sh
+++ ubuntubuild.sh
@@ -5,18 +5,18 @@ apt install jq fakeroot dpkg rpm git -y
 
 # Linux deb and rpm
 cp ubuntupackage.json package.json
-npm i -g @electron-forge/cli
-npm i -g @electron-forge/maker-squirrel
-npm i -g @electron-forge/maker-deb
-npm i -g @electron-forge/maker-rpm
-npm i -g @electron-forge/maker-zip
-npm i -g @electron-forge/maker-dmg
-npm i @electron-forge/cli
+# npm i -g @electron-forge/cli
+# npm i -g @electron-forge/maker-squirrel
+# npm i -g @electron-forge/maker-deb
+# npm i -g @electron-forge/maker-rpm
+# npm i -g @electron-forge/maker-zip
+# npm i -g @electron-forge/maker-dmg
 npm install
+# npm i @electron-forge/cli
 rm -rf out/make/*
 npm run make
 
-# Raspberry Pi:
-npm i -g node-deb
-cp rpipackage.json package.json
-node-deb --install-strategy npm-install -- src/
+# # Raspberry Pi:
+# npm i -g node-deb
+# cp rpipackage.json package.json
+# node-deb --install-strategy npm-install -- src/
