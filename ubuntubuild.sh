e1806c38c899c456ffad7a0a1b909164a068c08d
Russell VanderMey <russell@vandermey.us>
2026-01-13 17:27:00 -0500

    Add remark to explain why NODE_ENV export is after npm install

--- ubuntubuild.sh
+++ ubuntubuild.sh
@@ -6,7 +6,10 @@ apt install jq fakeroot dpkg rpm git -y
 # Linux deb and rpm
 cp ubuntupackage.json package.json
 npm install
+
+# ^ npm install runs without NODE_ENV=production, so it installs all dependencies including devDependencies (which contain @electron-forge/cli and the makers)
 export NODE_ENV=production
+
 rm -rf out/make/*
 npm run make
 
